313fc4821bc2a9538c8f245e5d4e4a728b61d74b
sunaydajahan <196670776+sunaydajahan@users.noreply.github.com>
2025-02-02 21:45:41 +0600

    Update java.js

--- java.js
+++ java.js
@@ -1,16 +1,4 @@
 let name = prompt("Who are you?", "your name");
 
 alert("Hello! " + name + " Welcome! ");
-let visitorDisplay = document.getElementById("visitor");
-let button2 = document.getElementById("button2");
-let visitor = localStorage.getItem("visitor")
-  ? parseInt(localStorage.getItem("visitor"))
-  : 0;
-visitorDisplay.textContent = visitor;
-
-button2.addEventListener("click", function () {
-  visitor++;
-  visitorDisplay.textContent = visitor;
-  localStorage.setItem("visitor", visitor);
-});
 
